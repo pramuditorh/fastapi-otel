068d06d321846c8758e67da6292a41612d7f0976
pramuditorh <pramuditorh@gmail.com>
2023-09-10 23:33:53 +0700

    add opentelemetry

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Depends
-from config import get_settings, Settings
-from api import read, write
+from app.config import get_settings, Settings
+from app.api import read, write
+from opentelemetry import trace
+from opentelemetry.exporter import jaeger
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 app = FastAPI()
 
@@ -13,3 +20,11 @@ async def root(settings: Settings = Depends(get_settings)):
         "message": "Hello World",
         "environment": settings.environment
     }
+
+resource = Resource(attributes={"service.name": "fastapi-otel"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+otlp_exporter = OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+FastAPIInstrumentor.instrument_app(app)
