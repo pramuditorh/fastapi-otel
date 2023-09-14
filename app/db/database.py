1d92c4cde722cec7aa6357fc6ab5f12d081bb470
pramuditorh <pramuditorh@gmail.com>
2023-09-14 09:47:18 +0700

    edit

--- app/db/database.py
+++ app/db/database.py
@@ -1,24 +1,50 @@
-import motor.motor_asyncio
+# import motor.motor_asyncio
+import pymongo
+from opentelemetry.instrumentation.pymongo import PymongoInstrumentor
 
 MONGO_URI = "mongodb://root:example@mongo:27017/"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+# client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+# database = client.dojo_otel
+# dojo_otel_collection = database.get_collection("dojo_otel_collection")
+
+# def collection_helper(data):
+#   return {
+#     "id": str(data["_id"]),
+#     "message": str(data["message"])
+#   }
+
+# async def read_db():
+#   read_data = []
+#   async for data in dojo_otel_collection.find():
+#     read_data.append(collection_helper(data))
+#   return read_data
+
+# async def write_db(write_data: dict):
+#   data = await dojo_otel_collection.insert_one(write_data)
+#   new_data = await dojo_otel_collection.find_one({"_id": data.inserted_id})
+#   return collection_helper(new_data)
+
+PymongoInstrumentor().instrument()
+
+client = pymongo.MongoClient(MONGO_URI)
 database = client.dojo_otel
-dojo_otel_collection = database.get_collection("dojo_otel_collection")
+dojo_otel_collection = database.dojo_otel_collection  # Use the collection directly
 
 def collection_helper(data):
-  return {
-    "id": str(data["_id"]),
-    "message": str(data["message"])
-  }
+    return {
+        "id": str(data["_id"]),
+        "message": str(data["message"])
+    }
 
-async def read_db():
-  read_data = []
-  async for data in dojo_otel_collection.find():
-    read_data.append(collection_helper(data))
-  return read_data
+def read_db():
+    read_data = []
+    cursor = dojo_otel_collection.find()
+    for data in cursor:
+        read_data.append(collection_helper(data))
+    return read_data
 
-async def write_db(write_data: dict):
-  data = await dojo_otel_collection.insert_one(write_data)
-  new_data = await dojo_otel_collection.find_one({"_id": data.inserted_id})
-  return collection_helper(new_data)
+def write_db(write_data: dict):
+    result = dojo_otel_collection.insert_one(write_data)
+    new_data = dojo_otel_collection.find_one({"_id": result.inserted_id})
+    return collection_helper(new_data)
